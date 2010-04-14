45599ccbdc66b3ee51816a4b8c732b2f688696d3
Thomas Rückstieß <ruecksti@in.tum.de>
2010-04-14 14:03:11 +0200

    added example for nfqca (not working yet?)

---
+++ examples/rl/valuebased/nfqca.py
@@ -0,0 +1,68 @@
+__author__ = 'Thomas Rueckstiess, ruecksti@in.tum.de'
+
+
+from pybrain.rl.environments.cartpole import CartPoleEnvironment, BalanceTask, JustBalanceTask, CartPoleRenderer
+from pybrain.rl.agents import LearningAgent
+from pybrain.rl.experiments import EpisodicExperiment
+from pybrain.rl.learners.valuebased import NFQCA, PolicyValueNetwork
+from pybrain.rl.explorers import NormalExplorer
+
+from numpy import array, arange, meshgrid, pi, zeros, mean
+from matplotlib import pyplot as plt
+
+render = False
+
+
+plt.ion()
+
+env = CartPoleEnvironment()
+if render:
+    renderer = CartPoleRenderer()
+    env.setRenderer(renderer)
+    renderer.start()
+
+module = PolicyValueNetwork(4, 1)
+
+task = JustBalanceTask(env, 50)
+learner = NFQCA()
+learner.explorer = NormalExplorer(1)
+
+agent = LearningAgent(module, learner)
+testagent = LearningAgent(module, None)
+experiment = EpisodicExperiment(task, agent)
+
+def plotPerformance(values, fig):
+    plt.figure(fig.number)
+    plt.clf()
+    plt.plot(values, 'o-')
+    plt.gcf().canvas.draw()
+
+
+performance = []
+
+pf_fig = plt.figure()
+
+# experiment.doEpisodes(50)
+    
+while(True):
+    experiment.doEpisodes(10)
+
+    # while agent.history.getNumSequences() > 100:
+    #     agent.history.removeSequence(0)
+        
+    agent.learn(1)  
+    
+    # test performance
+    if render:
+        env.delay = True
+    experiment.agent = testagent
+    r = mean([sum(x) for x in experiment.doEpisodes(20)])
+    env.delay = False  
+
+    performance.append(r) 
+    plotPerformance(performance, pf_fig)
+    print "reward avg", r
+    # print "exploration", agent.learner.explorer.epsilon
+    print "num samples", agent.history.getNumSequences()
+    # agent.reset()
+    
